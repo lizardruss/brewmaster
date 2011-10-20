365623b2204e9e2d6eb24c0d8bcc4a2c3e2c00b7
Russell Centanni <russell.centanni@gmail.com>
2011-10-19 20:43:23 -0400

    Initial Project Layout
    Set up Java & Flex Domain Libraries, and Web Application.

---
+++ brewmaster-flex-domain/src/main/java/org/brewmaster/flex/domain/App.java
@@ -0,0 +1,13 @@
+package org.brewmaster.flex.domain;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
