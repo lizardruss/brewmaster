d4813cf1a41f60203c1a4e8721128f8faa63820c
Russell Centanni <russell.centanni@gmail.com>
2012-06-22 17:19:02 -0400

    Formatted parsing files and fixed a duplicate constant.

--- brewmaster-data/src/main/java/org/brewmaster/parser/Buffer.java
+++ brewmaster-data/src/main/java/org/brewmaster/parser/Buffer.java
@@ -4,37 +4,31 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Buffer {
-	private List<String> lines;
-	
-	public Buffer()
-	{
-		super();
-		lines = new LinkedList<String>();
-	}
-	
-	public void clearBuffer()
-	{
-		lines.clear();
-	}
-	
-	public String readBuffer()
-	{
-		return lines.remove(0);
-	}
-	
-	public String readAll()
-	{
-		StringBuffer string = new StringBuffer();
-		for (String line : lines)
-		{
-			string.append(line);
-		}
-		lines.clear();
-		return string.toString();
-	}
-	
-	public void writeBuffer(String line)
-	{
-		lines.add(line);
-	}
+    private List<String> lines;
+
+    public Buffer() {
+        super();
+        lines = new LinkedList<String>();
+    }
+
+    public void clearBuffer() {
+        lines.clear();
+    }
+
+    public String readBuffer() {
+        return lines.remove(0);
+    }
+
+    public String readAll() {
+        StringBuffer string = new StringBuffer();
+        for (String line : lines) {
+            string.append(line);
+        }
+        lines.clear();
+        return string.toString();
+    }
+
+    public void writeBuffer(String line) {
+        lines.add(line);
+    }
 }
