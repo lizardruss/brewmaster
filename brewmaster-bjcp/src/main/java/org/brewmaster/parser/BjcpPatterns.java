5477a5e1845113d42696a357e08e0a936a3a76d2
Russell Centanni <russell.centanni@gmail.com>
2012-06-24 18:50:50 -0400

    -Renamed project to reflect its purpose.

---
+++ brewmaster-bjcp/src/main/java/org/brewmaster/parser/BjcpPatterns.java
@@ -0,0 +1,39 @@
+package org.brewmaster.parser;
+
+import java.util.regex.Pattern;
+
+public enum BjcpPatterns {
+    CATEGORY("\\d+\\.[\\p{Lu}\\(\\)\\s\\/\\/\\-]+"),
+    STYLE("\\d+\\p{Lu}\\.\\s\\p{Lu}[\\,\\p{L}\\(\\)\\s\\/\\/\\-\\p{Digit}]+"),
+    AROMA("^Aroma\\:[\\p{L}\\s\\p{Punct}\\p{Digit}]+$"),
+    AROMA_APPEARANCE_FLAVOR_MOUTHFEEL("^Aroma, appearance, flavor, mouthfeel[\\p{L}\\s\\p{Punct}\\p{Digit}]+$"),
+    APPEARANCE("^Appearance\\:[\\p{L}\\s\\p{Punct}\\p{Digit}]+$"),
+    FLAVOR("^Flavor\\:[\\p{L}\\s\\p{Punct}\\p{Digit}]+$"),
+    HISTORY("^History\\:[\\p{L}\\s\\p{Punct}\\p{Digit}]+$"),
+    MOUTHFEEL("^Mouthfeel\\:[\\p{L}\\s\\p{Punct}\\p{Digit}]+$"),
+    OVERALL_IMPRESSION("^Overall Impression\\:[\\p{L}\\s\\p{Punct}\\p{Digit}]+$"),
+    COMMENTS("^Comments\\:[\\p{L}\\s\\p{Punct}\\p{Digit}]+$"),
+    INGREDIENTS("^Ingredients\\:[\\s\\p{L}\\p{Punct}\\p{Digit}]+$"),
+    VITAL_STATISTICS("^Vital Statistics\\:[\\–\\p{L}\\s\\p{Punct}\\p{Digit}]+$"),
+    COMMERCIAL_EXAMPLES("^Commercial Examples\\:[\\p{L}\\s\\p{Punct}\\p{Digit}[^\u0000-\u0080]]+$");
+
+    private BjcpPatterns(String pattern) {
+        this.pattern = Pattern.compile(pattern);
+    }
+
+    private final Pattern pattern;
+
+    public Pattern getPattern() {
+        return pattern;
+    }
+
+    public boolean matches(String line) {
+        return pattern.matcher(line).matches();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(COMMERCIAL_EXAMPLES.matches("Commercial Examples: Pilsner Urquell, Krušovice Imperial 12°,"));
+        System.out.println(COMMERCIAL_EXAMPLES.matches("Commercial Examples: Pilsner Urquell, Krusovice Imperial 12°,"));
+        System.out.println(COMMERCIAL_EXAMPLES.matches("Commercial Examples: Pilsner Urquell, Krusovice Imperial 12,"));
+    }
+}
---
+++ brewmaster-bjcp/src/main/java/org/brewmaster/parser/ParserModes.java
@@ -0,0 +1,18 @@
+package org.brewmaster.parser;
+
+public class ParserModes {
+    public static final int READ_CATEGORY_OR_STYLE_TITLE = 0;
+    public static final int READ_CATEGORY_DESCRIPTION = 1;
+    public static final int READ_STYLE_DESCRIPTION = 2;
+    public static final int READ_AROMA = 3;
+    public static final int READ_APPEARANCE = 4;
+    public static final int READ_FLAVOR = 5;
+    public static final int READ_HISTORY = 6;
+    public static final int READ_MOUTHFEEL = 7;
+    public static final int READ_OVERALL_IMPRESSION = 8;
+    public static final int READ_COMMENTS = 9;
+    public static final int READ_INGREDIENTS = 10;
+    public static final int READ_VITAL_STATISTICS = 11;
+    public static final int READ_COMMERCIAL_EXAMPLES = 12;
+    public static final int READING = 13;
+}
